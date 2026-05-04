54faaab9ce1d0266ee6d1943a5805c2cdc991824
Zubaydulla Okhunboboyev <149357777+ZubaydullaCoder@users.noreply.github.com>
2026-05-04 20:07:35 +0500

    update intro experience wording

--- components/intro.tsx
+++ components/intro.tsx
@@ -63,10 +63,9 @@ export default function Intro() {
         animate={{ opacity: 1, y: 0 }}
       >
         <span className="font-bold">Hello, I'm Zubaydulla.</span> I'm a{" "}
-        <span className="font-bold">React.Js/Next.js developer</span> with{" "}
-        <span className="font-bold">1.5 years</span> of experience. I enjoy
-        building <span className="italic">sites & apps</span>. My focus is{" "}
-        <span className="underline">React (Next.js)</span>.
+        <span className="font-bold">React/Next.js developer</span> with hands-on
+        experience building <span className="italic">sites & apps</span>. My
+        focus is <span className="underline">React (Next.js)</span>.
       </motion.h1>
 
       <motion.div
